9d363406d26ea0a0ecfd878b761d54709cca44a2
CryptoSD100 <157069818+CSD100@users.noreply.github.com>
2024-03-19 16:41:12 -0400

    Update contractAddresses.ts

--- consts/contractAddresses.ts
+++ consts/contractAddresses.ts
@@ -1,6 +1,6 @@
 export const nftDropContractAddress =
-  "0xF5482b04C2bBfe7AF667805b26f09193fcB1dF89";
+  "0xC9EaF24C5d80b627549e51f27b5427aCA64757a2";
 export const tokenContractAddress =
   "0x55d398326f99059fF775485246999027B3197955";
 export const stakingContractAddress =
-  "0xc4cA80118ff296f319CE45286afE660a4E28fF93";
+  "0x93772B97204183237E34f1e661aba4f57d88f91E";
